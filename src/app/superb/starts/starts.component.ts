af98ec7a68afe53c93028085fe76312865250d67
gottaBoy <460632205@qq.com>
2017-12-03 23:06:21 +0800

    ngChanges钩子，添加中间人模式

--- src/app/superb/starts/starts.component.ts
+++ src/app/superb/starts/starts.component.ts
@@ -1,21 +1,44 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, Output,EventEmitter} from '@angular/core';
+import { OnChanges, SimpleChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-starts',
   templateUrl: './starts.component.html',
   styleUrls: ['./starts.component.css']
 })
-export class StartsComponent implements OnInit {
+export class StartsComponent implements OnInit, OnChanges {
 
   @Input()
   rating:number = 0;
+  @Output()
+  ratingChange:EventEmitter<number> = new EventEmitter();
+  @Input()
+  readonly:boolean = true;
   stars:boolean[];
   constructor() { }
 
   ngOnInit() {
+    // this.stars = [];
+    // for(let i = 1; i <= 5; i++){
+    //   this.stars.push(i > this.rating);
+    // }
+  }
+
+  ngOnChanges(changes:SimpleChanges):void {
     this.stars = [];
     for(let i = 1; i <= 5; i++){
       this.stars.push(i > this.rating);
+    }
+  }
+
+  clickStar(index:number){
+    if(!this.readonly){
+      this.rating = index + 1;
+      // this.stars = [];
+      // for(let i = 1; i <= 5; i++){
+      //   this.stars.push(i > this.rating);
+      // }
+      this.ratingChange.emit(this.rating);
     }
   }
 
